dc6354b392baed72423f1c733dbd6b0fc517f6b4
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-08-04 11:17:18 +0900

    [feature] support certfile and keyfile (#11)
    * [feature] support certfile and keyfile
    
    fixes #7
    
    * x509 suite does work with freebsd-10.3-amd64
    
    * confirmed ubuntu 16.04 works with x509
    
    * workaround checksum mismatch
    
    * avoid 0.12.1 until the upstream re-release
    
    * confirmed ansible-ubuntu-14.04-amd64 works
    
    * confirmed 14.04 with 2.3.1.0-1ppa~trusty works
    
    * remove version restrictions
    
    as all jenkins slaves have installed 0.4.0
    
    * remove the version constraint for workaround
    
    faraday_middleware 0.12.2 has been released

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -127,3 +127,9 @@ ports.each do |p|
     it { should be_listening }
   end
 end
+
+describe command("echo | openssl s_client -connect 127.0.0.1:3000 -showcerts") do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should match(/verify error:num=18:self signed certificate/) }
+  its(:stdout) { should match(/#{Regexp.escape("issuer=/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd/CN=foo.example.org")}/) }
+end
